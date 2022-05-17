33d405a80589d5d5fe5b300adc7e5536307375f9
Rob Skilling <rob@dxw.com>
2022-05-17 11:18:59 +0100

    Add class that produces array of chapter nav items
    To generate the navigation that is required for long-read items, we need
    two things:
    
    - Navigation to the other "Chapters" of the long-read (i.e. the parent
     and sibling posts of the current post, or its children if it is the
    parent post)
    - Navigation to the headings within the current page
    
    This class generates an array of objects that can be used to generate
    the first of these, with each object having a `title` and `url`
    property.
    
    For: https://trello.com/c/KuFLpg6U/570-%F0%9F%8E%AF-goal-implement-long-read-template-for-lambeth-together-and-enable-gutenberg-for-this

---
+++ spec/chapter_navigation.spec.php
@@ -0,0 +1,93 @@
+<?php
+
+use Kahlan\Arg;
+
+describe(\LongReadPlugin\ChapterNavigation::class, function () {
+    beforeEach(function () {
+        $this->chapterNavigation = new \LongReadPlugin\ChapterNavigation();
+    });
+
+    describe('->getItems()', function () {
+        context('global post is the parent long-read post', function () {
+            it('returns an array of items with the first item as the current post, which will have a null url property', function () {
+                global $post;
+                $post = (object) [
+                    'ID' => 123,
+                    'post_title' => 'Chapter One'
+                ];
+                allow('get_post_parent')->toBeCalled()->andReturn(false);
+                allow('get_posts')->toBeCalled()->andReturn([
+                    (object) [
+                        'ID' => 456,
+                        'post_title' => 'Chapter Two'
+                    ],
+                    (object) [
+                        'ID' => 789,
+                        'post_title' => 'Chapter Three'
+                    ],
+                ]);
+                expect('get_posts')->toBeCalled()->once()->with([
+                    'post_parent' => 123,
+                    'post_type' => 'long-read',
+                    'posts_per_page' => -1,
+                    'orderby' => 'menu_order',
+                    'order' => 'ASC'
+                ]);
+                allow('get_permalink')->toBeCalled()->andReturn('http://chapter-two-link', 'http://chapter-three-link');
+
+                $result = $this->chapterNavigation->getItems();
+
+                expect(count($result))->toEqual(3);
+                expect($result[0]->title)->toEqual('Chapter One');
+                expect($result[0]->url)->toEqual(null);
+                expect($result[1]->title)->toEqual('Chapter Two');
+                expect($result[1]->url)->toEqual('http://chapter-two-link');
+                expect($result[2]->title)->toEqual('Chapter Three');
+                expect($result[2]->url)->toEqual('http://chapter-three-link');
+            });
+        });
+
+        context('global post is a child long-read post', function () {
+            it('returns an array of items with the current post in the appropriate place, and with a null url property', function () {
+                global $post;
+                $post = (object) [
+                    'ID' => 456,
+                    'post_title' => 'Chapter Two'
+                ];
+                $parentPost = (object) [
+                    'ID' => 123,
+                    'post_title' => 'Chapter One'
+                ];
+                allow('get_post_parent')->toBeCalled()->andReturn($parentPost);
+                allow('get_posts')->toBeCalled()->andReturn([
+                    $post = (object) [
+                        'ID' => 456,
+                        'post_title' => 'Chapter Two'
+                    ],
+                    (object) [
+                        'ID' => 789,
+                        'post_title' => 'Chapter Three'
+                    ]
+                ]);
+                expect('get_posts')->toBeCalled()->once()->with([
+                    'post_parent' => 123,
+                    'post_type' => 'long-read',
+                    'posts_per_page' => -1,
+                    'orderby' => 'menu_order',
+                    'order' => 'ASC'
+                ]);
+                allow('get_permalink')->toBeCalled()->andReturn('http://chapter-one-link', 'http://chapter-three-link');
+
+                $result = $this->chapterNavigation->getItems();
+
+                expect(count($result))->toEqual(3);
+                expect($result[0]->title)->toEqual('Chapter One');
+                expect($result[0]->url)->toEqual('http://chapter-one-link');
+                expect($result[1]->title)->toEqual('Chapter Two');
+                expect($result[1]->url)->toEqual(null);
+                expect($result[2]->title)->toEqual('Chapter Three');
+                expect($result[2]->url)->toEqual('http://chapter-three-link');
+            });
+        });
+    });
+});
