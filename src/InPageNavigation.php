bd63d0c5dce49c950fb8f5703294e8fb40920f6d
patdel0 <zefiru@protonmail.com>
2026-03-11 09:56:15 +0000

    dom document

--- src/InPageNavigation.php
+++ src/InPageNavigation.php
@@ -7,35 +7,64 @@ class InPageNavigation
 	/** @var array $inPageNavItems */
 	private $inPageNavItems = [];
 
-	private function parseHeading(string $html): object
-	{
-		$matches = [];
-		preg_match('/(id=")(.*?)"/', $html, $matches);
-		return (object) [
-			'title' => trim(strip_tags($html)),
-			'id' => $matches[2]
-		];
-	}
+	// private function parseHeading(string $html): object
+	// {
+	// 	$matches = [];
+	// 	preg_match('/(id=")(.*?)"/', $html, $matches);
+	// 	return (object) [
+	// 		'title' => trim(strip_tags($html)),
+	// 		'id' => $matches[2]
+	// 	];
+	// }
 
-	private function findHeadingBlocks(array $blocks): void
-	{
-		foreach ($blocks as $block) {
-			if ($block['blockName'] == 'core/heading'  && array_key_exists('attrs', $block) && (!isset($block['attrs']['level']) || $block['attrs']['level'] == 2)) {
-				if (empty(trim(strip_tags($block['innerHTML'])))) {
-					continue;
-				}
-				$this->inPageNavItems[] = $this->parseHeading($block["innerHTML"]);
-			} elseif ($block['blockName'] == 'acf/group-block' || $block['blockName'] == 'core/group') {
-				$this->findHeadingBlocks($block['innerBlocks']);
-			}
-		}
-	}
+	// private function findHeadingBlocks(array $blocks): void
+	// {
+	// 	foreach ($blocks as $block) {
+	// 		if ($block['blockName'] == 'core/heading'  && array_key_exists('attrs', $block) && (!isset($block['attrs']['level']) || $block['attrs']['level'] == 2)) {
+	// 			if (empty(trim(strip_tags($block['innerHTML'])))) {
+	// 				continue;
+	// 			}
+	// 			$this->inPageNavItems[] = $this->parseHeading($block["innerHTML"]);
+	// 		} elseif ($block['blockName'] == 'acf/group-block' || $block['blockName'] == 'core/group') {
+	// 			$this->findHeadingBlocks($block['innerBlocks']);
+	// 		}
+	// 	}
+	// }
+
+	// public function getItems(): array
+	// {
+	// 	global $post;
+	// 	$blocks = parse_blocks($post->post_content);
+	// 	$this->findHeadingBlocks($blocks);
+	// 	return $this->inPageNavItems;
+	// }
 
 	public function getItems(): array
 	{
+		return $this->findHeadingBlocks();
+	}
+
+	private function findHeadingBlocks(): array
+	{
 		global $post;
-		$blocks = parse_blocks($post->post_content);
-		$this->findHeadingBlocks($blocks);
+
+		$dom = new \DOMDocument();
+		$dom->loadHTML($post->post_content);
+
+		$h2s = $dom->getElementsByTagName('h2');
+		foreach ($h2s as $h2) {
+			$id = $h2->getAttribute('id');
+
+			if (!$id) {
+				$id = sanitize_title($h2->textContent);
+			}
+
+			$this->inPageNavItems[] = (object)[
+				'title' => trim($h2->textContent),
+				'id' => $id,
+			];
+		}
+
 		return $this->inPageNavItems;
 	}
 }
