7eb1c77cf4efa2cd3f656fb976b96f2b5c278d48
Rob Skilling <rob@dxw.com>
2026-05-28 13:29:49 +0100

    Release v2.4.1

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
  * Plugin URI: https://github.com/dxw/long-read-plugin
  * Description: Adds the underlying functionality to enable long-reads
  * Author: dxw
- * Version: 2.4.0
+ * Version: 2.4.1
  * Network: false
  */
 
