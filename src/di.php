0ac74548226cb3ecd767af457f16233a115efef0
Sim Brody <sim@dxw.com>
2026-07-08 14:18:05 +0100

    New up Parent Title with chosen approach in di.php

--- src/di.php
+++ src/di.php
@@ -3,8 +3,8 @@
 $registrar->addInstance(new \LongReadPlugin\PostType());
 $registrar->addInstance(new \LongReadPlugin\HeadingAnchors());
 $registrar->addInstance(new \LongReadPlugin\Navigation(
-	new LongReadPlugin\HierarchicalChapterNavigation(),
-	new LongReadPlugin\HierarchicalInPageNavigation()
+	new \LongReadPlugin\HierarchicalChapterNavigation(),
+	new \LongReadPlugin\HierarchicalInPageNavigation()
 ));
 $registrar->addInstance(new \LongReadPlugin\Options());
 $registrar->addInstance(new \LongReadPlugin\Template());
