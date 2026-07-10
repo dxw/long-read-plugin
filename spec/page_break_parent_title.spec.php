1201e8d68628876f1c2598d66a3f3c595e7c53d8
Sim Brody <sim@dxw.com>
2026-07-10 13:02:38 +0100

    Add class to determine long read parent title for page break method

---
+++ spec/page_break_parent_title.spec.php
@@ -0,0 +1,27 @@
+<?php
+
+describe(LongReadPlugin\PageBreakParentTitle::class, function () {
+	beforeEach(function () {
+		$this->parentTitleRetriever = new \LongReadPlugin\PageBreakParentTitle();
+	});
+
+	it('implements the ParentTitleRetrieverInterface', function () {
+		expect($this->parentTitleRetriever)->toBeAnInstanceOf(\LongReadPlugin\ParentTitleRetrieverInterface::class);
+	});
+
+	describe('->get()', function () {
+		it('returns the title of the parent post', function () {
+			global $post;
+			$post = (object) [
+				'ID' => 123
+			];
+
+			allow('get_the_title')->toBeCalled()->andReturn('Parent Post Title');
+			expect('get_the_title')->toBeCalled()->with($post);
+
+			$result = $this->parentTitleRetriever->get();
+
+			expect($result)->toEqual('Current Post Title');
+		});
+	});
+});
